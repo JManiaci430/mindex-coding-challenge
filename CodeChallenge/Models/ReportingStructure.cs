2bfa6b2b9ed81514ce0dced73d1e0d585becafa4
Justin Maniaci <justin.maniaci@dlp.org>
2024-03-28 16:13:09 -0400

    Task 1 Completed and Unit Tests Written

---
+++ CodeChallenge/Models/ReportingStructure.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CodeChallenge.Models
+{
+    public class ReportingStructure
+    {
+        public String Employee { get; set; }
+        public int NumberOfReports { get; set; }
+    }
+}
